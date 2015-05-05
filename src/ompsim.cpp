75d0971cd5076d4ae6e2f604db1e69eb0c2b482d
Carlos Pizano <cpu@chromium.org>
2015-05-04 20:31:56 -0700

    it sort of runs
    Not clear if correct but now 100 'persons' do awake. The goal atm is to
    have 100 people wake, have breakfast and get to the office.

--- src/ompsim.cpp
+++ src/ompsim.cpp
@@ -9,6 +9,7 @@
 #include <queue>
 #include <functional>
 #include <memory>
+#include <random>
 
 // Represents time in microseconds. That is about 292K years.
 class SimTime {
@@ -29,11 +30,19 @@ public:
   static const SimTime& now() { return *g_master; }
 
   SimTime() : st_(0) {}
-  explicit SimTime(int seconds) : st_(100 * seconds) {}
   explicit SimTime(int64_t u_seconds) :st_(u_seconds) {}
   bool operator==(const SimTime& other) const { return st_ == other.st_; }
   bool operator<(const SimTime& other) const { return st_ < other.st_; }
   bool operator>(const SimTime& other) const { return st_ > other.st_; }
+
+  SimTime& operator+=(int64_t u_seconds) {
+    st_ += u_seconds;
+    return *this;
+  }
+
+  friend SimTime operator+(const SimTime& time, int64_t u_seconds) {
+    return SimTime(time) += u_seconds;
+  }
 };
 
 SimTime* SimTime::g_master = nullptr;
@@ -72,14 +81,33 @@ public:
 
 class Scheduler {
   std::priority_queue<SimEvent, std::vector<SimEvent>, std::greater<SimEvent>> pqueue_;
+  std::random_device randdev_;
+  std::mt19937 randgen_;
 
 public:
+  Scheduler() : randgen_(randdev_()) { }
+
   bool empty() const { return pqueue_.empty(); }
 
   void run_top() { 
     auto& ev = pqueue_.top();
     ev.run(this);
     pqueue_.pop();
+  }
+
+  void add_event(int delta_seconds, Actor* actor, uint32_t id) {
+    auto activation_time = SimTime::now() + (100 * uint64_t(delta_seconds));
+    pqueue_.emplace(activation_time, actor, id);
+  }
+
+  SimTime random_uniform_seconds(int min_sec, int max_sec) {
+    std::uniform_int_distribution<uint64_t> dis(min_sec, max_sec);
+    return SimTime(100 * dis(randgen_));
+  }
+
+  int random_uniform_number(int min, int max) {
+    std::uniform_int_distribution<> dis(min, max);
+    return dis(randgen_);
   }
 };
 
@@ -123,10 +151,35 @@ namespace OMP {
 
 class Person : public Actor {
   SimTime age_;
+  int energy_;   // calories.
+  int weight_;   // lbs.
 
 public:
-  Person(Scheduler* sched) {
+  enum States {
+    dead,
+    sleeping,
+    awake,
+    eating,
+    moving,
+    working,
+    relaxing,
+  };
 
+  Person() = delete;
+
+  Person(Scheduler* sched)
+      : age_(0),
+        energy_(0),
+        weight_(0) {
+
+    int khour = 60 * 60;
+    int kyear = 365 * 24 * khour;
+    age_ = sched->random_uniform_seconds(kyear * 20, kyear * 90);
+    weight_ = sched->random_uniform_number(90, 220);
+    energy_ = 2 * (weight_ * 13);
+
+    auto wakeup_time = sched->random_uniform_number(4, 8) * khour;
+    sched->add_event(wakeup_time, this, awake);
   }
 
 protected:
@@ -139,7 +192,9 @@ class OMPSim : public Simulation {
   std::vector<std::unique_ptr<Person>> people_;
 
   int prime(Scheduler* sched) override {
-
+    for (int ix = 0; ix != 100; ++ix) {
+      people_.push_back(std::make_unique<Person>(sched));
+    }
     return 0;
   }
 };
@@ -147,9 +202,24 @@ class OMPSim : public Simulation {
 
 }  // namepsace OMP.
 
+DWORD WINAPI SpawnSimulation(void* ctx) {
+  long mode = 1;
+  if (ctx) {
+    OMP::OMPSim simulation;
+    auto rv = simulation.run(10000);
+    return rv;
+  } else {
+    auto th = ::CreateThread(NULL, 0, &SpawnSimulation, &mode, 0, nullptr);
+    return th? 1 : 0;
+  }
+}
+
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE,
                     wchar_t* cmd_line,
                     int cmd_show) {
+  if (!SpawnSimulation(nullptr))
+    return -1;
+
   MSG msg;
 
   while (::GetMessage(&msg, NULL, 0, 0)) {
